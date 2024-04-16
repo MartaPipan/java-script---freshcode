cf2b6663eaacdb9e23075fdc1e8629b110a2488c
Marta Prystatska <tradlisboa@gmail.com>
2024-04-16 22:01:00 +0100

    Add example relational

--- file.js/file.js
+++ file.js/file.js
@@ -1,31 +1,18 @@
-/**
- * pryklady pidnesennia do stepeni
- */
-let number1 = 1;
-let number2 = 4;
+let number1 = 10;
+let number2 = 10;
 //number1 = number1 + number2;
 number1 += number2;
+const resultLess = number1 < number2;
+console.log(resultLess);
+console.log(number1 >= number2);
 
-console.log('prefix increment =', ++number1);
-console.log('number2 =', number2++);
-console.log('number2 =', number2);
-
-const base1 = 2;
-const pow1 = 3;
-const result1 = base1 ** pow1;
-console.log('result1', result1);
 //number1 = number1 / 3;
 number1 /= 3;
+let str1 = 'cat';
+let str2 = 'category';
+let str3 = '102';
+let str4 = '2';
 
-const base2 = 2;
-const pow2 = -3;
-const result2 = base2 ** pow2;
-console.log('result2', result2);
-
-const base3 = 2;
-const pow3 = 3;
-const result3 = (-base3) ** pow3;
-console.log('result3', result3);
 //number1 = number1 * number2;
 number1 *= number2;
 
@@ -39,5 +26,11 @@ number1 %= 5;
 number1--;
 
 number1 = (number1 + 2) * 3 + number1;
-
+console.log(str1 > str2);
+console.log(number1 > str3);
+console.log(number1 > true);
+console.log(number1 > undefined);
+console.log(number1 > null);
+console.log(number1 > NaN);
+console.log(str1 > NaN);
 
