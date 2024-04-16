2c3bd8f6771227f5c72ad9d281e665eff20e63a7
Marta Prystatska <tradlisboa@gmail.com>
2024-04-16 21:32:05 +0100

    Add example exceptions

--- file.js/file.js
+++ file.js/file.js
@@ -1,66 +1,28 @@
-// console.log('ageAnimal = ', ageAnimal);
-// console.log('type ageAnimal = ',typeof ageAnimal);
-const numberFromDB = 4;
-const numberFromUser = '11';
-const conversionNumberFromUser =  Number(numberFromUser);
-const result = numberFromDB +  conversionNumberFromUser;
-console.log('result =', result);
-console.log('typeof result =', typeof result);
-console.log('typeof conversionNumberFromUser =', typeof conversionNumberFromUser);
-
-let ageAnimal;
-console.log('ageAnimal = ', ageAnimal);
-console.log('type ageAnimal = ', typeof ageAnimal);
-ageAnimal = 2;
-console.log('ageAnimal = ', ageAnimal);
-console.log('type ageAnimal = ', typeof ageAnimal);
-ageAnimal = 'one';
-console.log('ageAnimal = ', ageAnimal);
-console.log('type ageAnimal = ', typeof ageAnimal);
 const isActiveUser = false;
 const result2 = numberFromDB + Number(isActiveUser);
 console.log('result =', result2);
 console.log('typeof result =', typeof result2);
+console.log(12 % 0); //NaN  
+console.log(12*'qwerty'); //NaN
 
-const amountPawsAnimal = 4;
-const AMOUNT_DAYS_WEEK = 7;
+console.log(12 + -true); //12
 
-// створіть змінні для:
-// - ім'я користувача (Маша)
-const userFirstName = 'Маша';
-// - вік користувача (23)
-const userAge = 23;
-// - назва мови програмування (С++)
-const nameProgrammingLanguage = 'C++';
-// - чи повнолітній користувач  (true)
-const isAdultUser = true;
-// - користувач громодянство України (null)
-const citizenshipUkraine = null;
-// - рік народження користувача (undefined)
-let userYearBirthday;
+console.log(12 * undefined); //NaN
 
-console.log('title book: "qwerty" ');
-console.log("I'm a teacher");
-console.log("I'm a teacher");
-
-const login = 'qwerty';
-
-{
-  const login = 'asdfgh';
-  console.log('login = ', login);
-}
-{
-  const login = 'wwww';
-  console.log('login = ', login);
-}
-{
-  const login = 'eeeee';
-  console.log('login = ', login);
-}
-
-console.log('login = ', login);
-
-const userFirstName = 'Bred';
-const userLastName = 'Pitt';
-const userFullName = userFirstName + ' ' + userLastName;
-console.log('userFullName =', userFullName);
+console.log(12 - NaN); //NaN
+/*
+0 -0 +0
+''
+null
+false
+*/
+/*
+1
+true
+*/
+/*
+NaN
+0/0
+n%0
+/+-*% NaN || undefined
+*/
