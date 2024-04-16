167174b7c70f4f20a3dfce8fbd6b02b170d9912d
Marta Prystatska <tradlisboa@gmail.com>
2024-04-16 22:05:55 +0100

    Add logical NOT

--- file.js/file.js
+++ file.js/file.js
@@ -1,5 +1,39 @@
 let number1 = 10;
 let number2 = '10';
+
 console.log(number1 === number2); //false
 console.log(number1 !== number2); //true
+
+const isAdult = true;
+
+// !  NOT
+
+const resultNot = !isAdult;
+
+console.log(resultNot);
+
+const userAge = 23;  //true
+console.log(!userAge);
+
+/*
+false
+0 -0 +0
+''
+undefined
+null
+NaN
+*/
+
+
+let quantity = 15;
+const isExists = !!quantity;
+
+
+
+
+
+
+0 comments on commit ff7cb7c
+@MartaPipan
+Comment
 
