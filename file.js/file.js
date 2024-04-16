b05a22464e9d7b3e9d2fae243233c1d237c760dd
Marta Prystatska <tradlisboa@gmail.com>
2024-04-16 22:20:31 +0100

    Add getSumTwoNumbers after refactoring and decomposition

--- file.js/file.js
+++ file.js/file.js
@@ -1,9 +1,11 @@
-const userValue = prompt('enter number', 5);
 //написати функцію яка рахує суму двох чисел які ввів користувач
 
 const getSumTwoNumbersFromUser = function () {
   const userValue1 = prompt('enter number', 2);
   const userValue2 = prompt('enter number', 3);
+const checkUserInputError = function (value) {
+  return value === null || value === '' || Number.isNaN(Number(value));
+};
 
   const checkUserInputError =
     userValue1 === null ||
@@ -12,15 +14,25 @@ const getSumTwoNumbersFromUser = function () {
     userValue2 === '' ||
     Number.isNaN(Number(userValue1)) ||
     Number.isNaN(Number(userValue2));
+const getSumTwoNumbers = function (number1, number2) {
+  return number1 + number2;
+};
 
   if (checkUserInputError) {
+const logResult = function (value1, value2) {
+  if (checkUserInputError(value1) || checkUserInputError(value2)) {
     console.log('error');
   } else {
     const summa = Number(userValue1) + Number(userValue2);
     console.log(userValue1, ' + ', userValue2, ' = ', summa);
   }
+    return; //output from function
+  } 
+  console.log(value1, ' + ', value2, ' = ',
+    getSumTwoNumbers(Number(value1), Number(value2))
+  );
 };
 
 getSumTwoNumbersFromUser();
 
-console.log(userValue, typeof userValue);
+//logResult(prompt('enter number', 2), prompt('enter number', 3));
