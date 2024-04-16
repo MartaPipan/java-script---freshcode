e1342df0524d1738258569ff3c7189de842ad2c9
Marta Prystatska <tradlisboa@gmail.com>
2024-04-16 21:55:18 +0100

    Add example any assignment

--- file.js/file.js
+++ file.js/file.js
@@ -2,15 +2,20 @@
  * pryklady pidnesennia do stepeni
  */
 let number1 = 1;
-let number2 = 2;
+let number2 = 4;
+//number1 = number1 + number2;
+number1 += number2;
 
 console.log('prefix increment =', ++number1);
 console.log('number2 =', number2++);
 console.log('number2 =', number2);
+
 const base1 = 2;
 const pow1 = 3;
 const result1 = base1 ** pow1;
 console.log('result1', result1);
+//number1 = number1 / 3;
+number1 /= 3;
 
 const base2 = 2;
 const pow2 = -3;
@@ -21,6 +26,18 @@ const base3 = 2;
 const pow3 = 3;
 const result3 = (-base3) ** pow3;
 console.log('result3', result3);
+//number1 = number1 * number2;
+number1 *= number2;
 
+//number1 = number1 ** 2;
+number1 **= 2;
+
+//number1 = number1 % 5
+number1 %= 5;
+
+//number1 = number1 - 1;
+number1--;
+
+number1 = (number1 + 2) * 3 + number1;
 
 
