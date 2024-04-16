fbdd852fd181ea94f3413e1c660b52519b816206
Marta Prystatska <tradlisboa@gmail.com>
2024-04-16 22:19:01 +0100

    Add getSumTwoNumbersFromUser version 1.0

--- file.js/file.js
+++ file.js/file.js
@@ -1,21 +1,26 @@
-logMessage1();
-//declaration
-function logMessage1() {
-  console.log('hi, from function 1');
-}
-logMessage1();
 const userValue = prompt('enter number', 5);
+//написати функцію яка рахує суму двох чисел які ввів користувач
 
+const getSumTwoNumbersFromUser = function () {
+  const userValue1 = prompt('enter number', 2);
+  const userValue2 = prompt('enter number', 3);
 
-//expression
-const logMessage2 = function () {
-  console.log('hi, from function 2');
+  const checkUserInputError =
+    userValue1 === null ||
+    userValue2 === null ||
+    userValue1 === '' ||
+    userValue2 === '' ||
+    Number.isNaN(Number(userValue1)) ||
+    Number.isNaN(Number(userValue2));
+
+  if (checkUserInputError) {
+    console.log('error');
+  } else {
+    const summa = Number(userValue1) + Number(userValue2);
+    console.log(userValue1, ' + ', userValue2, ' = ', summa);
+  }
 };
-logMessage2();
 
-//arrow
-const logMessage3 =  () => {
-  console.log('hi, from function 3 arrow');
-};
-logMessage3();
+getSumTwoNumbersFromUser();
+
 console.log(userValue, typeof userValue);
