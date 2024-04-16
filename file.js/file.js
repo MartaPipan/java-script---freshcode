d79588056a42af9059ba9f1fe177840574b736d4
Marta Prystatska <tradlisboa@gmail.com>
2024-04-16 21:29:01 +0100

    Add example explicit and implicit data type casting

--- file.js/file.js
+++ file.js/file.js
@@ -1,5 +1,12 @@
 // console.log('ageAnimal = ', ageAnimal);
 // console.log('type ageAnimal = ',typeof ageAnimal);
+const numberFromDB = 4;
+const numberFromUser = '11';
+const conversionNumberFromUser =  Number(numberFromUser);
+const result = numberFromDB +  conversionNumberFromUser;
+console.log('result =', result);
+console.log('typeof result =', typeof result);
+console.log('typeof conversionNumberFromUser =', typeof conversionNumberFromUser);
 
 let ageAnimal;
 console.log('ageAnimal = ', ageAnimal);
@@ -10,6 +17,10 @@ console.log('type ageAnimal = ', typeof ageAnimal);
 ageAnimal = 'one';
 console.log('ageAnimal = ', ageAnimal);
 console.log('type ageAnimal = ', typeof ageAnimal);
+const isActiveUser = false;
+const result2 = numberFromDB + Number(isActiveUser);
+console.log('result =', result2);
+console.log('typeof result =', typeof result2);
 
 const amountPawsAnimal = 4;
 const AMOUNT_DAYS_WEEK = 7;
@@ -48,3 +59,8 @@ const login = 'qwerty';
 }
 
 console.log('login = ', login);
+
+const userFirstName = 'Bred';
+const userLastName = 'Pitt';
+const userFullName = userFirstName + ' ' + userLastName;
+console.log('userFullName =', userFullName);
