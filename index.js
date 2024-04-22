3c412a89d55d65a3bee3a45305b10199e7f79cc0
Marta Prystatska <tradlisboa@gmail.com>
2024-04-22 16:50:51 +0100

    Add create object

--- index.js
+++ index.js
@@ -30,3 +30,19 @@ const getMultTwoNums2 = function () {
 function areaRect(side1 = 5, side2 = 8) {
   return getMultTwoNums(side1, side2);
 }
+
+const object1 = new Object();
+object1.prop1 = 'prop1';
+const object2 = Object(7);
+const object = {}; //new Object()
+const button = {
+  text: 'click',
+};
+button.width = 50;
+button.bgcolor = 'blue';
+
+console.log(button);
+
+
+const symbol = '*';
+//symbol = '-';
