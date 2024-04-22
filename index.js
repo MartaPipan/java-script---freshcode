53bbb220198515460a0b081517eb6197162a1d66
Marta Prystatska <tradlisboa@gmail.com>
2024-04-22 16:54:53 +0100

    Add copy by reference

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-//чисті функції - детерміновані без побічних ефектів
-/**
- *
- * @param {number} number1
- * @param {number} number2
- * @returns
- */
-function getMultTwoNums(number1 = 5, number2 = 7) {
-  return number1 * number2;
-}
-function getSumTwoNums(number1 = 5, number2 = 7) {
-  return number1 + number2;
-}
-//HOF - hight order function
-function calc (number1, number2, functionOperation){
-  return functionOperation(number1, number2);
-}
-// debugger
-calc(4, 3, getMultTwoNums) /// 4,3, f5dd72
-const calc2 = function (number1, number2, functionOperation) {
-    return functionOperation(number1, number2);
-}
-//не чиста
-const getMultTwoNums2 = function () {
-  const number1 = prompt();
-  const number2 = prompt();
-  return number1 * number2;
-};
-
-function areaRect(side1 = 5, side2 = 8) {
-  return getMultTwoNums(side1, side2);
-}
 
 const object1 = new Object();
 object1.prop1 = 'prop1';
@@ -38,8 +6,19 @@ const object = {}; //new Object()
 const button = {
   text: 'click',
 };
+const user = {
+  //222
+  fname: 'Brad',
+  sname: 'Pitt',
+};
 button.width = 50;
 button.bgcolor = 'blue';
+
+const activeUser = user; //222
+activeUser.age = 60;
+
+const userClient = activeUser; //222
+userClient.order = '123';
 
 console.log(button);
 
