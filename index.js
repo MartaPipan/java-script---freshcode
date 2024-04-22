0aaf365462b0ff40d3edfe2fa68df6b6152c849f
Marta Prystatska <tradlisboa@gmail.com>
2024-04-22 16:58:43 +0100

    Add user with methods

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-
-const object1 = new Object();
-object1.prop1 = 'prop1';
-const object2 = Object(7);
-const object = {}; //new Object()
-const button = {
-  text: 'click',
-};
 const user = {
   //222
   fname: 'Brad',
   sname: 'Pitt',
+  password: 'qwerty',
+  sayName: function(){
+    return 'Hi, I\'m '+ user.fname;
+  },
+  saySecondName: function(){
+    return `Hi, I'm ${user.sname}`;
+  }
 };
-button.width = 50;
-button.bgcolor = 'blue';
-
-const activeUser = user; //222
-activeUser.age = 60;
-
-const userClient = activeUser; //222
-userClient.order = '123';
-
-console.log(button);
-
-
-const symbol = '*';
-//symbol = '-';
+console.log(user);
+user.age = 60;
+user.isAdult = true;
+console.log(user.age);
+delete user.password;
+console.log(user);
+console.log(user.saySecondName());
