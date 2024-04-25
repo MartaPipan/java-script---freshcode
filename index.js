38e9217c7b0adc168212ef71397f92068624cc12
Marta Prystatska <tradlisboa@gmail.com>
2024-04-25 13:17:16 +0100

    Add getMultTable

--- index.js
+++ index.js
@@ -72,3 +72,39 @@ const prop = '45';
 if (prop in user) {
   console.log(typeof user[prop] === 'function' ? user[prop]() : user[prop]);
 }
+
+//ключем об'єкта може бути рядок або символ
+/*
+const multTable = {
+  '1 * 1 = ': 1,
+  '1 * 2 = ': 2,
+  '1 * 3 = ': 3,
+  '1 * 4 = ': 4,
+  '1 * 5 = ': 5,
+  '1 * 6 = ': 6,
+}
+*/
+function getMultTable(firstNumber = 1) {
+  const multTable = {};
+  for (let i = 1; i <= 10; i++) {
+    multTable[`${firstNumber} * ${i} = `] = firstNumber * i;
+  }
+  return multTable;
+}
+console.log(getMultTable(5));
+const userTask = '5 * 3 = ';
+const multTable = getMultTable(5);
+
+console.log(multTable[userTask]);
+
+function getMultTableMany(min = 1, max = 9) {
+  const multTable = {};  //address references
+  for (let firstNumber = min; firstNumber <= max; firstNumber++) {
+    for (let i = 1; i <= 10; i++) {
+      multTable[`${firstNumber} * ${i} = `] = firstNumber * i;
+    }
+  }
+  return multTable;  //address references
+}
+
+console.log(getMultTableMany());
