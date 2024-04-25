846065b46005bd6188f929e648c41138da9348f9
Marta Prystatska <tradlisboa@gmail.com>
2024-04-25 13:36:45 +0100

    Create MyArray.js

--- index.js
+++ index.js
@@ -129,3 +129,20 @@ console.log(myArrNums);
 const myArrNums2 = new MyArray();
 myArrNums2.push(1000);
 console.log(myArrNums.push === myArrNums2.push);
+
+
+
+//конструктор для прототипу
+function MyArrayPrototype() {
+  this.push = function (value) {
+    this[this.length] = value;
+    this.length++;
+    return this.length;
+  };
+}
+//конструктор для данних (для сутності інстанса)
+function MyArray() {
+  this.length = 0;
+}
+// зв'язок між конструктором для данних та об'єктом проптотипу
+MyArray.prototype = new MyArrayPrototype();
