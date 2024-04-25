2c5337b14ee5f9bb44eb260602efc77bedc5c62c
Marta Prystatska <tradlisboa@gmail.com>
2024-04-25 13:26:17 +0100

    Add example object proptotype

--- index.js
+++ index.js
@@ -27,3 +27,36 @@ console.log(arrNums instanceof Array);
 console.log(arrNums instanceof Number);
 
 console.log(Array.isArray(arrNums));
+
+
+
+
+
+const arrNums = [];
+arrNums.push(7);
+arrNums.push(77);
+console.log(arrNums);
+const arrNums2 = [];
+console.log(arrNums.push === arrNums2.push);
+
+const myArrayPrototype = {};
+myArrayPrototype.push = function (value) {
+  this[this.length] = value; 
+  this.length++;
+  return this.length;
+};
+
+function MyArray() {
+  this.length = 0;
+}
+
+MyArray.prototype = myArrayPrototype;
+
+
+const myArrNums = new MyArray();
+myArrNums.push(20);
+myArrNums.push(33);
+console.log(myArrNums);
+const myArrNums2 = new MyArray();
+myArrNums2.push(1000);
+console.log(myArrNums.push === myArrNums2.push);
