97e64a1384c94e920e5f3be390680961b5c4c60f
Marta Prystatska <tradlisboa@gmail.com>
2024-04-25 13:41:41 +0100

    Add __proto__ & prototype

---
+++ file.js/MyArray.js
@@ -0,0 +1,17 @@
+//конструктор для прототипу
+function MyArrayPrototype() {
+  this.push = function (value) {
+    this[this.length] = value;
+    this.length++;
+    return this.length;
+  };
+  this.pop = function () {};
+  this.forEach = function () {};
+}
+//конструктор для данних (для сутності інстанса)
+function MyArray() {
+  this.length = 0;
+}
+// зв'язок між конструктором для данних та об'єктом проптотипу
+MyArray.prototype = new MyArrayPrototype();
+
