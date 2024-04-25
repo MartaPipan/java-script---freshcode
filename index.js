86e5fff544af814d8a672f45f20bc52cd2e6e2d0
Marta Prystatska <tradlisboa@gmail.com>
2024-04-25 02:09:15 +0100

    homework:Computed member access

--- index.js
+++ index.js
@@ -1,62 +1,20 @@
-// Написати функцію, яка буде повертати об'єкт родина.
-// Ключ - член родини
-// Значення - ім'я
-// В родині повинно бути як мінімум 2 особи, можна запитати в користувача скільки особ в його родині.
-// І всі данні (і ключ і значення) вносяться через prompt
 
-// const family = {
-//     'mather': 'Olena',
-//     'cat': 'Ray',
-// }
 
-// function createFamily() {
-//     let numberOfMembers;
-//     while (true) {
-//         const input = prompt('How many members are there in your family?');
-//         numberOfMembers = Number(input);
-//         if (input === null || Number.isNaN(numberOfMembers) || numberOfMembers < 2) {
-//             console.log('Please enter a valid number (minimum 2).');
-//         } 
-//         else {
-//             break;
-//         }
-//     }
-
-//     let role;
-//     let name;
-//     let family = {};
-//     for (let i = 0; i < numberOfMembers; i++) {
-//         let role, name;
-
-//         while (true) {
-//            let role = prompt(`What is the role of family member ${i + 1}?`);
-//             if (role === null || Number.isNaN(role)) {
-//                 console.log('Please enter a valid role (not a number).');
-//             }
-//             else {
-//                 break;
-//             }
-//         }
-
-//         while (true) {
-//             let name = prompt(`What is the name of the ${role}?`);
-//             if (Number.isNaN(name) ||
-//                 typeof name === 'number' ||
-//                 name === null) {
-//                 console.log('Invalid input. Please enter a valid name (not a number).');
-//             }
-//             else {
-//                 break;
-//             }
-//         }
-
-//         family[role] = name;
-//     }
-//     return family;
-// }
-
-// const family = createFamily();
-// console.log(family)
+/**
+ * Написати функцію, яка буде повертати об'єкт родина.
+*Ключ - член родини
+*Значення - ім'я
+*В родині повинно бути як мінімум 2 особи, можна запитати в користувача скільки особ в його родині.
+*І всі данні (і ключ і значення) вносяться через prompt
+*const family = {
+*    'mather': 'Olena',
+*    'cat': 'Ray',
+*}
+ * @param {number} numberOfMembers
+ * @param {string} role
+ * @param {string} nome
+ * @returns {Object} family
+ */
 
 
 function createFamily() {
@@ -71,12 +29,11 @@ function createFamily() {
             break;
         }
     }
-
-    let family = {};
+    const family = {};
     for (let i = 0; i < numberOfMembers; i++) {
         let role;
         while (true) {
-            role = prompt(`What is the role of family member ${i + 1}?`);
+           role = prompt(`What is the role of family member ${i + 1}?`);
             if (!(/^[a-z][a-z]*$/.test(role)) ||
                 role === null ||
                 Number.isNaN(role) ||
@@ -106,6 +63,5 @@ function createFamily() {
     }
     return family;
 }
-
 const family = createFamily();
 console.log(family);
