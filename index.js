5b32d4a11e706924e9a90fff97debbdebbafffa7
Marta Prystatska <tradlisboa@gmail.com>
2024-04-28 15:00:28 +0100

    Add homework Shopping list (methods)

--- index.js
+++ index.js
@@ -61,3 +61,65 @@ function createFamily() {
 }
 const family = createFamily();
 console.log(family);
+
+
+
+/**Початковий список покупок 
+const shoppingList = ["milk", "eggs", "bread"];
+Всі завдання виконувати в наведеному порядку, після кожного завдання логувати отриманий результат
+Використовувати лише перелічені методи: pop, push, shift, unshift, slice, splice, indexOf, lastIndexOf
+- Додайте новий елемент в кінець списку 
+- Видаліть останній елемент зі списку
+- Додайте новий елемент на початок списку 
+- Видаліть перший елемент зі списку
+- Створіть повну копію списку покупок 
+- Знайдіть індекс  "bread"
+- Заменіть "bread" на "muffin"
+- Знайдіть індекс  "bread" останнє входження
+- Після молока додайте "meat", "fish"
+- Видаліть "eggs"
+*/
+
+const shoppingList = ['milk', 'eggs', 'bread'];
+//Додайте новий елемент в кінець списку    (push; splice)
+console.log(shoppingList.push('cookies'));
+//console.log(shoppingList.splice(-1,0,'cookies'));
+
+// Видаліть останній елемент зі списку
+console.log(shoppingList.pop());
+//console.log(shoppingList.splice(-1,1));
+//console.log(shoppingList(shoppingList.length, 1));
+
+//Додайте новий елемент на початок списку 
+console.log(shoppingList.unshift('potato'));
+//console.log(shoppingList.splice(0,0,'potato'));
+
+// Видаліть перший елемент зі списку
+console.log(shoppingList.shift());
+//console.log(shoppingList.splice(0, 1));
+
+//Створіть повну копію списку покупок 
+const shoppingListCopy = shoppingList.slice()
+console.log(shoppingListCopy);
+
+//Знайдіть індекс  "bread"
+console.log(shoppingList.indexOf('bread'));
+
+//Заменіть "bread" на "muffin"
+console.log(shoppingList.splice(2, 1, 'muffin'));
+
+//Знайдіть індекс  "bread" останнє входження
+console.log(shoppingList.lastIndexOf('bread'));
+
+//Після молока додайте "meat", "fish"
+console.log(shoppingList.splice(0, 0, 'meat', 'fish'));
+
+//Видаліть "eggs"
+//console.log(shoppingList);     in console: (5) ['meat', 'fish', 'milk', 'eggs', 'muffin']
+console.log(shoppingList.splice(-2, 1));
+
+/**or 
+console.log(shoppingList.indexOf('eggs'));     in console: index3
+console.log(shoppingList.splice(3, 1));
+*/
+
