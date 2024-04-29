63faf52c37b84c766800e8db55f1004a0ee731e1
Marta Prystatska <tradlisboa@gmail.com>
2024-04-29 08:03:31 +0100

    add example for Each method

--- index.js
+++ index.js
@@ -179,3 +179,19 @@ arrNums1.forEach(function (elem){console.log(elem**2)})
 logPow2(11)
 console.log('--------------');
 myArrNums1.forEach(logPow2);
+
+
+const users = [
+  {id:100, login:'qwe1', isMale: true},
+  {id:200, login:'qwe21', isMale: true},
+  {id:101, login:'asd', isMale: false},
+  {id:178, login:'sss', isMale: false},
+  {id:10, login:'max', isMale: true},
+]
+
+users.forEach(function(user){
+  user.isSubscribe = true;
+  user.wishList = [];
+})
+
+console.table(users)
